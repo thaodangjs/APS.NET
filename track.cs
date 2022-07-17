f3d7c2a94e753a1a16e074e12ca283d5a1b9b4ad
Thao Dang <104821090+thaodangjs@users.noreply.github.com>
2022-07-17 11:19:39 +0700

    Create track.cs

---
+++ track.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace HelloWorld
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            /*
+                Phương thức ConfigureServices cho phép truy cập đến các dịch vụ, dependency được Inject vào
+                Webhost. Hoặc bạn cũng có thể đưa thêm các dependency tại đây.
+            */
+
+        }
+
+        // IHostingEnvironment  env cho phép truy cập các biến môi trường, thư mục nguồn, thư mục file.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
